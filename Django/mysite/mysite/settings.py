2c615e25fd956c12a179ef35e4efada275e89227
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2024-09-03 00:26:17 +0530

    added changes to settings.py

--- Django/mysite/mysite/settings.py
+++ Django/mysite/mysite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Members',
+    'Buses',
+    'Tickets'
 ]
 
 MIDDLEWARE = [
